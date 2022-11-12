27ca74be3519028e9cf6f4b6e7b1608b7d4eb204
Josemedinan <118068779+Josemedinan@users.noreply.github.com>
2022-11-12 10:35:20 -0600

    Add files via upload

---
+++ firebase/firebase.ino
@@ -0,0 +1,208 @@
+
+
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme;
+
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "HITRON-F5A0"
+#define WIFI_PASSWORD "josema230806"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyBY_1qF37tZLWSaHNgfMNvkKzQt-kcR7ZI"
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL "jmmnahr@gmail.com"
+#define USER_PASSWORD "Josema123"
+
+// Insert RTDB URLefine the RTDB URL
+#define DATABASE_URL "https://esp32-prueba23-default-rtdb.firebaseio.com/"
+
+// Define Firebase objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// Variable to save USER UID
+String uid;
+
+// Variables to save database paths
+String databasePath;
+String tempPath;
+String humPath;
+String presPath;
+
+// BME280 sensor
+//Adafruit_BME280 bme; // I2C
+float temperature;
+float humidity;
+float pressure;
+float contador =1.0;
+
+// Timer variables (send new readings every three minutes)
+unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = 5000;
+
+// Initialize BME280
+/*void initBME(){
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+}*/
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+// Write float values to the database
+void sendFloat(String path, float value){
+  if (Firebase.RTDB.setFloat(&fbdo, path.c_str(), value)){
+    Serial.print("Writing value: ");
+    Serial.print (value);
+    Serial.print(" on the following path: ");
+    Serial.println(path);
+    Serial.println("PASSED");
+    Serial.println("PATH: " + fbdo.dataPath());
+    Serial.println("TYPE: " + fbdo.dataType());
+  }
+  else {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+
+  // Initialize BME280 sensor
+  //initBME();
+  initWiFi();
+
+  // Assign the api key (required)
+  config.api_key = API_KEY;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid;
+
+  // Update database path for sensor readings
+  tempPath = databasePath + "/temperature"; // --> UsersData/<user_uid>/temperature
+  humPath = databasePath + "/humidity"; // --> UsersData/<user_uid>/humidity
+  presPath = databasePath + "/pressure"; // --> UsersData/<user_uid>/pressure
+
+
+  while (!Serial);
+  Serial.println(F("BME680 test"));
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
+}
+
+void loop(){
+  // Send new readings to database
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    
+
+    // Get latest sensor readings
+   /* temperature = bme.readTemperature();
+    humidity = bme.readHumidity();
+    pressure = bme.readPressure()/100.0F;*/
+
+    ///////COMENTAN ESTA SECCION SI DESEAN UTILIZAR EL BME////////////////
+    //  if(contador>20){contador = 1.0;}
+     // contador=contador+1;
+    //temperature = contador*0.1;
+    //humidity = contador*0.2;
+    //pressure = contador*0.3;
+
+
+    // Send readings to database:
+    sendFloat(tempPath, bme.temperature);
+    sendFloat(humPath, bme.humidity);
+    sendFloat(presPath, bme.pressure / 100.0);
+  }
+  if (! bme.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.humidity);
+  Serial.println(" %");
+}
